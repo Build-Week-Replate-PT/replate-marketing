6cb8e315f921e431cecb8e8ef12b00857d04d0d1
Janessa Garrow <janessa.garrow@gmail.com>
2019-10-19 12:43:46 -0600

    Added js script tag to head of all html pages

--- index.js
+++ index.js
@@ -10,18 +10,13 @@ window.addEventListener('scroll', (e) => {
 
 window.addEventListener('load', e => {
     const checkbox = document.getElementById('menu-btn');
-    console.log('i am in here');
 
     checkbox.addEventListener('change', function() {
         if (this.checked) {
             const header = document.getElementById('header');
-            console.log('here')
 
             document.addEventListener('click', function closeMenu(e) {
-                console.log(e);
                 let isClickInside = header.contains(e.target);
-
-                console.log(isClickInside);
 
                 if (!isClickInside) {
                     checkbox.checked = false;
