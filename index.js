7a6c1002b6b0e53e7d7c801d7e67c129b62ad26c
Janessa Garrow <janessa.garrow@gmail.com>
2019-10-19 15:47:00 -0600

    Added hover color styling to the nav for the hamburger menu. Edited index.js so that the header box shadow goes away after closing the menu (if it's at the top of the site)

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ window.addEventListener('load', e => {
     const checkbox = document.getElementById('menu-btn');
 
     checkbox.addEventListener('change', function() {
+        const head = document.querySelector('header');
+
         if (this.checked) {
+
+            if (!head.classList.contains('box-shadow')) {
+                head.classList.add('box-shadow');
+            }
+
             const header = document.getElementById('header');
 
             document.addEventListener('click', function closeMenu(e) {
@@ -20,9 +27,16 @@ window.addEventListener('load', e => {
 
                 if (!isClickInside) {
                     checkbox.checked = false;
+
+                    if (window.scrollY === 0) {
+                        head.classList.remove('box-shadow');
+                    }
+
                     document.removeEventListener('click', closeMenu);
                 }
             })
+        } else if (!this.checked && window.scrollY == 0 && head.classList.contains('box-shadow')) {
+            head.classList.remove('box-shadow');
         }
     })
 });
